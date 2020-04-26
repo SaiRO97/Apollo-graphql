950d6e997bc185332905c2c80c7e49981359347f
Ruslan <ruslan@MacBook-ProCatalina.local>
2020-04-26 20:47:07 +0600

    created test movie schemas

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const grapqlHTTP = require("express-graphql");
+const graphqlSchema = require("./graphql/schema/schema");
 
 const app = express();
 const PORT = 3001;
 
-app.use("/graphql", grapqlHTTP({}));
+app.use(
+  "/graphql",
+  grapqlHTTP({
+    schema: graphqlSchema,
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, (error) => {
   error ? console.error(error) : console.log(`Server is running at ${PORT} `);
