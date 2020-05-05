ce99159520f710c9378bb2f6d7337c7975ae9887
Ruslan <stonetiger1997@gmail.com>
2020-05-05 22:05:49 +0600

    starting create mutation

--- server/app.js
+++ server/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const grapqlHTTP = require("express-graphql");
 const graphqlSchema = require("./graphql/schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 3001;
+
+mongoose.connect(
+  "mongodb+srv://ruslan:Ruslan424540575600@todo-express-a2lpt.mongodb.net/",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 app.use(
   "/graphql",
@@ -12,6 +18,10 @@ app.use(
     graphiql: true,
   })
 );
+
+const dbConnection = mongoose.connection;
+dbConnection.on("error", (err) => console.log(`Connection error ${err}`));
+dbConnection.once("open", () => console.log("Connected to DB"));
 
 app.listen(PORT, (error) => {
   error ? console.error(error) : console.log(`Server is running at ${PORT} `);
--- server/graphql/schema/schema.js
+++ server/graphql/schema/schema.js
@@ -1,12 +1,42 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+} = graphql;
+const Movie = require("../../models/movie");
+const Director = require("../../models/director");
 
 const MovieType = new GraphQLObjectType({
   name: "Movie",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    director: {
+      type: DirectorType,
+      resolve(parent, args) {
+        return Director.findOne(parent.directorId);
+      },
+    },
+  }),
+});
+
+const DirectorType = new GraphQLObjectType({
+  name: "Director",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    movies: {
+      type: new GraphQLList(MovieType),
+      resolve(parent, args) {
+        return Movie.find({ directorId: parent.id });
+      },
+    },
   }),
 });
 
@@ -15,8 +45,27 @@ const Query = new GraphQLObjectType({
   fields: {
     movie: {
       type: MovieType,
-      args: { id: { type: GraphQLString } },
-      resolve(parent, args) {},
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Movie.findById(args.id);
+      },
+    },
+    movies: {
+      type: new GraphQLList(MovieType),
+      resolve: () => {
+        return Movie.find({});
+      },
+    },
+    director: {
+      type: DirectorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Director.findById(args.id);
+      },
+    },
+    directors: {
+      type: new GraphQLList(DirectorType),
+      resolve: () => Director.find({}),
     },
   },
 });
